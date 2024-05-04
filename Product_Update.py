00e292113be7c0ee1607c4aaef31873453196a39
ianshimabukuro <shimakun000@gmail.com>
2024-05-04 22:00:27 +0900

    Added comments, removed function definition, organized

--- Product_Update.py
+++ Product_Update.py
@@ -8,9 +8,11 @@ import os.path
 import csv
 import google.auth.exceptions
 
+#Initialize shopify API session
 session = shopify.Session(shop_url, api_version, ac_tok)
 shopify.ShopifyResource.activate_session(session)
 
+#Gather product list and make a csv file
 products_list = []
 print("Start retrieving")
 products = get_all_resources(shopify.Product)
@@ -20,6 +22,7 @@ for product in products:
 products_df = pd.DataFrame(products_list)
 products_df.to_csv('products.csv', index=False)
 
+#Get credentials for Google API usage
 print("Authenticating Google API...")
 try:
     creds=getCred(SCOPES)
@@ -29,10 +32,13 @@ except google.auth.exceptions.RefreshError:
     os.remove("token.json")
     creds = getCred(SCOPES)
     print("Credentials Refreshed and Adquired")
-#Step 3
+
+#Write to specified Google Sheets
 print("Writing to Products Google Sheet")
 with open('products.csv', encoding="utf8") as f:
     reader = csv.reader(f)
     tp = list(tuple(line) for line in reader)
 UpdateValue(creds, spreadsheetID=GoogleSheetID, range='Products!A1', values=tp)
+
+#Delete created csv file
 os.remove("C:/Users/akio_/PycharmProjects/ShopifyAPIGSExporter/products.csv")
