67908aab152f49bbb99a0160b6a50db25c10bac2
ianshimabukuro <shimakun000@gmail.com>
2024-05-04 22:04:59 +0900

    Added comments and organized

--- OAuth2.py
+++ OAuth2.py
@@ -1,10 +1,15 @@
 from shopify_modules.auth_functions import*
 from urllib.parse import urlparse, parse_qs
 from selenium import webdriver
+import binascii
+import os.path
 
+#Set initial state
 state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
 shopify.Session.setup(api_key=client_id, secret=client_secret)
 session = shopify.Session("socalization.myshopify.com", '2024-01')
+
+#Get the url
 url = getauthcodeurl(session,state)
 
 
@@ -15,16 +20,18 @@ webdriver_path = '/chromedriver'
 driver = webdriver.Chrome()
 driver.get(url)
 
+#Use selenium to log into shopify
 LoginShopify(driver)
 time.sleep(10)
 
+#Get the current URL with contains the params
 updated_url=driver.current_url
-
 parsed_url = urlparse(updated_url)
 
 # Get the query parameters as a dictionary
 query_params = {key: values[0] for key, values in parse_qs(parsed_url.query).items()}
 print(query_params)
 
+#Use params to exchange for access_token
 access_token = authcode_for_accesstoken(session,query_params)
 print(access_token)
