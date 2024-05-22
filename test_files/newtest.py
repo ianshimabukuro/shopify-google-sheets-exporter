cf535fbd47557bb5882efeefafcd030a313c3a29
ianshimabukuro <shimakun000@gmail.com>
2024-05-23 00:52:56 +0900

    Now Inventory update has sku and id

--- test_files/newtest.py
+++ test_files/newtest.py
@@ -1,39 +1,5 @@
-import shopify
-from ids_passwords_strings.values import*
-import time
-
-
-def get_all_resources(resource_type, **kwargs):
-    resource_count = resource_type.count(**kwargs)
-    resources = []
-    if resource_count > 0:
-        page=resource_type.find(**kwargs)
-        resources.extend(page)
-        while page.has_next_page():
-            page = page.next_page()
-            resources.extend(page)
-    return resources
 
 
 
-
-
-session = shopify.Session(shop_url, api_version, ac_tok)
-shopify.ShopifyResource.activate_session(session)
-shop = shopify.Shop.current()
-
-Locations = shopify.Location.find()
-inventory = []
-for location in Locations:
-    print("next location")
-    Inventory_Level=shopify.Location.inventory_levels(location)
-    inventory.extend(Inventory_Level)
-    while Inventory_Level.has_next_page():
-        time.sleep(0.5)
-        print("next Page")
-        Inventory_Level = Inventory_Level.next_page()
-        inventory.extend(Inventory_Level)
-
-print(inventory)
 
 
--- test_files/test2.py
+++ test_files/test2.py
@@ -9,6 +9,7 @@ import os.path
 import csv
 import google.auth.exceptions
 locations_ids=[43039096968, 60906668168, 60919185544, 45357269128,65552973960]
+inventoryItemId=34766209908872
 session = shopify.Session(shop_url, api_version, ac_tok)
 shopify.ShopifyResource.activate_session(session)
 
@@ -16,7 +17,6 @@ shopify.ShopifyResource.activate_session(session)
 Locations = shopify.Location.find()
 inventory_list = []
 for location in Locations:
-    print("current location",location)
     Inventory_Levels = shopify.Location.inventory_levels(location)
     for inventory_level in Inventory_Levels:
         id = inventory_level.attributes["inventory_item_id"]
@@ -25,32 +25,9 @@ for location in Locations:
         inventory_list.append(inventory_item.attributes)
     while Inventory_Levels.has_next_page():
         Inventory_Levels = Inventory_Levels.next_page()
-        print("next page")
         for inventory_level in Inventory_Levels:
             id = inventory_level.attributes["inventory_item_id"]
             inventory_item = shopify.InventoryItem.find(id)
             time.sleep(0.2)
             inventory_list.append(inventory_item.attributes)
-
-inventory_df = pd.DataFrame(inventory_list)
-inventory_df.to_csv('inventory.csv', index=False)
-
-print("Authenticating Google API...")
-try:
-    creds=getCred(SCOPES)
-    print("Credentials Refreshed and Adquired")
-except google.auth.exceptions.RefreshError:
-    print("Refresh Error, deleting token.json")
-    os.remove("../token.json")
-    creds = getCred(SCOPES)
-    print("Credentials Refreshed and Adquired")
-#Step 3
-print("Writing to Inventory Google Sheet")
-with open('inventory.csv',encoding="utf8") as f:
-    reader = csv.reader(f)
-    ti = list(tuple(line) for line in reader)
-
-UpdateValue(creds, spreadsheetID=GoogleSheetID, range='Inventory!A1', values=ti)
-
-os.remove(project_path + "/run_files/inventory.csv")
 
