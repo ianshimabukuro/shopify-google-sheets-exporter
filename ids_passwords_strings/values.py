e91914d483e7633a4e1a6d678ffecac34ddd30d5
Ian Shimabukuro <shimakun000@gmail.com>
2024-11-17 15:14:16 -0800

    Update values.py

--- ids_passwords_strings/values.py
+++ ids_passwords_strings/values.py
@@ -1,10 +1,20 @@
-client_id = 'b852af00a43353437953ccd5d8462b1c'
-client_secret =  'c02180a8fb050a026d05004f8229437c'
-redirect_uri = 'https://www.socalization.com/'
-shop_url = 'socalization.myshopify.com'
-api_version ='2024-01'
-ac_tok="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"
-SCOPES="https://www.googleapis.com/auth/spreadsheets"
-GoogleSheetID='1iE7KHppmiHuP54wb6QJwuJ-KLJyS_QR93pji8nu2eiA'
+#Shopify API info
+
+client_id = ''
+client_secret =  ''
+redirect_uri = ''
+shop_url = ''
+api_version =''
+
+#Google API info
+
+#Account Token
+ac_tok= ""
+#API Scopes
+SCOPES= ""
+#ID of the Google Sheet
+GoogleSheetID=''
+
+#Running it locally, the path to main project folder
 #using forward slash
-project_path = 'C:/Users/akio_/PycharmProjects/ShopifyAPIGSExporter'
+project_path = ''
